3da0db8e300c82d559669e8d4e24e43585b77df8
Oskar Sundberg <oskarsundberg@live.se>
2024-02-15 09:13:41 +0100

    Mer grejer

--- test/src/index.js
+++ test/src/index.js
@@ -1,9 +1,53 @@
-const app = require('express')();
+const express = require("express");
+const axios = require("axios");
+//
+// Starts the microservice.
+//
+async function startMicroservice(port) {
+    // Creates an instance of the Express Web Server.
+    const app = express();
 
-app.get('/test', (req, res ) => 
-    res.json({ message: 'Docker and azure is easy' }) 
-);
+    // Registers a HTTP GET route for test
+    app.get("/test", async (req, res) => { // Route for streaming video.
+        console.log(`Hello World!`);
+    });
 
-const port = process.env.PORT || 8080;
+    // Starts the HTTP server.
+    app.listen(port, () => {
+        console.log(`Microservice online.`);
+    });
+}
 
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
+//
+// Application entry point.
+//
+async function main() {
+    // Throws an error if the PORT environment variable is missing.
+    if (!process.env.PORT) {
+        throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+    }
+
+    // Extracts the PORT environment variable.
+    const PORT = process.env.PORT;
+
+    // Starts the microservice
+    await startMicroservice(PORT);
+}
+
+//
+// Run microservice normally or under test.
+//
+if (require.main === module) {
+    // Only start the microservice normally if this script is the "main" module.
+    main()
+        .catch(err => {
+            console.error("Microservice failed to start.");
+            console.error(err && err.stack || err);
+        });
+}
+else {
+    // Otherwise we are running under test
+    module.exports = {
+        startMicroservice
+    };
+}
